2f08e0184a140db1fe4867066c2f12ce3bb8220c
Pradhum80 <kumarroney154@gmail.com>
2026-02-14 18:31:20 +0530

    Delete : JDBC

---
+++ Delete3.java
@@ -0,0 +1,46 @@
+import java.sql.*;
+
+public class Delete3 {
+    public static void main(String[] args) throws ClassNotFoundException {
+        
+        String url = "jdbc:mysql://localhost:3306/newjdbc";
+        String username = "root";
+        String password = "0069";
+        String query = "DELETE FROM employees where id = 2;" ;
+
+        try{
+        Class.forName("com.mysql.jdbc.Driver");  // Loaded Driver
+        System.out.println("Driver Loaded SuccessFully");
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+        }
+
+
+        try {
+            
+            Connection con = DriverManager.getConnection(url, username, password);  // Conection Established
+            System.out.println("Connection Esatblished SuccessFully !");
+
+            // Statement interface...
+            Statement stmt = con.createStatement();
+            int rowsaffected = stmt.executeUpdate(query); // delete 
+
+            if(rowsaffected > 0) {
+                System.out.println("Deletion SuccessFully : " + rowsaffected + "row(s) affected. ");
+
+            } else {
+                System.out.println("Deletion failed!!");
+            }
+
+            stmt.close();
+            con.close();
+            System.out.println("Connection close Successfully");
+
+
+        } 
+        catch(Exception e2) {
+            e2.printStackTrace();
+        }
+    }
+}
